354bbb1c56c866418a28f9d6c4d566a7b81e7afb
luis ramirez <luis.ramirez0001@outlook.com>
2023-06-08 21:46:23 -0500

    creo una nueva carpeta en donde se haran ejercicios sin ayuda empezando de los mas basicos e ir subiendo el nivel clear

---
+++ ejercicios/solitario1.js
@@ -0,0 +1,34 @@
+//Pide la edad y si es mayor de 18 años indica que ya puede conducir si es menor indicale que no puede.
+
+let edad = Number(prompt('ingresa tu edad'));
+
+if(edad <= 17){
+    document.write(' No Puedes Conducir');
+}
+
+else{
+    document.write('Puedes conducir')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
