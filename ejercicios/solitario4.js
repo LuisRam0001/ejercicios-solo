400dd69e5ddb6669358937f7802ff2787eb365ce
luis ramirez <luis.ramirez0001@outlook.com>
2023-06-09 21:22:15 -0500

    ejercicio ciclo for imprima 10 veces algo

---
+++ ejercicios/solitario4.js
@@ -0,0 +1,10 @@
+//Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces. NOTA: utiliza un ciclo para imprimir la frase las 10 veces.
+
+
+
+let frase = prompt('ingrese una frase');
+
+
+for( i = 0;  i<= 9; i++){
+    document.write( frase, '<br/>');
+}
